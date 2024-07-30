9e0ea7185e9e9bffdac62b8e34ae873588e58a43
Debadatta Hembram <debadattahembram@gmail.com>
2024-07-30 22:09:31 +0530

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,47 +23,51 @@ public class Main {
        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
        String req = in.readLine();
 
-       String path = req.split(" ")[1];;
 
-       System.out.println(path);
+       if(req != null){
+           String path = req.split(" ")[1];;
+
+           System.out.println(path);
 
 
-       String notFound = "HTTP/1.1 404 Not Found\r\n\r\n";
-       if( path.equals("/")){
-         client.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-       }else if(path.equals("/user-agent")){
-           String line;
-           while(!(line = in.readLine()).isEmpty()){
-               if(line.startsWith("User-Agent")){
-                   break;
+           String notFound = "HTTP/1.1 404 Not Found\r\n\r\n";
+           if( path.equals("/")){
+               client.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+           }else if(path.equals("/user-agent")){
+               String line;
+               while(!(line = in.readLine()).isEmpty()){
+                   if(line.startsWith("User-Agent")){
+                       break;
+                   }
                }
+
+               String msg = "";
+               if(!line.isEmpty()){
+                   msg = line.split(": ")[1];
+               }
+               String body = String.format(
+
+                       "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+
+                       msg.length(), msg);
+
+               client.getOutputStream().write(body.getBytes());
+           }else if (path.startsWith("/echo/")){
+               String msg = path.split("/")[2];
+               String body = String.format(
+
+                       "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+
+                       msg.length(), msg);
+
+               client.getOutputStream().write(body.getBytes());
+
+           }else{
+               client.getOutputStream().write(notFound.getBytes());
            }
-
-           String msg = "";
-           if(!line.isEmpty()){
-               msg = line.split(": ")[1];
-           }
-           String body = String.format(
-
-                   "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
-
-                   msg.length(), msg);
-
-           client.getOutputStream().write(body.getBytes());
-       }else if (path.startsWith("/echo/")){
-            String msg = path.split("/")[2];
-            String body = String.format(
-
-                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
-
-                    msg.length(), msg);
-
-            client.getOutputStream().write(body.getBytes());
-
-       }else{
-           client.getOutputStream().write(notFound.getBytes());
+       } else{
+           client.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
        }
-
        client.close();
        serverSocket.close();
      } catch (IOException e) {
