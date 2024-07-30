723eaa4b3c198e3c3fc26a81c0e8bff27e06978b
Debadatta Hembram <debadattahembram@gmail.com>
2024-07-30 20:44:21 +0530

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,23 +21,34 @@ public class Main {
        System.out.println("accepted new connection");
 
        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-
        String req = in.readLine();
 
        String path = "";
        if(req != null && !req.isEmpty()){
          path = req.split(" ")[1];
        }
+
        System.out.println("Req  : " + req);
        System.out.println("Path : "+path);
 
        String ok = "HTTP/1.1 200 OK\r\n\r\n";
        String notFound = "HTTP/1.1 404 Not Found\r\n\r\n";
-
-       if(path.equals("/")){
-         client.getOutputStream().write(ok.getBytes());
-       }else{
+       if(path.isEmpty()){
          client.getOutputStream().write(notFound.getBytes());
+       }else{
+         String[] paths = path.split("/");
+         if(paths.length < 2 || !paths[1].equals("echo")){
+           client.getOutputStream().write(notFound.getBytes());
+         }else{
+            String body = "";
+            if(paths.length>=3){
+              body = paths[2];
+            }
+            ok += "Content-Type: text/plain\r\n\r\n";
+            ok += "Content-Length: "+body.length()+"\r\n\r\n";
+            ok += body;
+            client.getOutputStream().write(ok.getBytes());
+         }
        }
 
        client.close();
